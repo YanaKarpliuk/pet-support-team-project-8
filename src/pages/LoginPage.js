10b7b9fa0c629bd759c2c5ecff3b81c31abcdf7a
Alona-Liubchenko <aylin888@rambler.ru>
2023-02-05 12:41:29 +0200

    з1

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,15 @@
+const LoginPage = () => {
+ return (
+  <div
+   style={{
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "85vh",
+   }}
+  >
+   <h1 style={{ textAlign: "center" }}>LoginPage</h1>
+  </div>
+ );
+};
+export default LoginPage;
---
+++ src/theme.js
@@ -0,0 +1,46 @@
+const theme = {
+ media: {
+ mobile: "(max-width: 767px)",
+  tablet: "(max-width: 1279px) and (min-width: 768px)",
+  desctop: "(min-width: 1280px)",
+ },
+ colors: {
+  fon: "#FDF7F2",
+  black: "#111111",
+  white: "#FFFFFF",
+  accent: "#F59256",
+ },
+ space: [0, 2, 4, 8, 16, 32, 64, 128, 256],
+
+ fonts: {
+  logo: "Poppins, sans-serif",
+  manrope: "Manrope, sans-serif",
+  inter: "Inter, sans-serif",
+ },
+ fontSizes: {
+  xs: "12px",
+  s: "14px",
+  m: "16px",
+  l: "32px",
+  xl: "64px",
+ },
+ fontWeights: {
+  normal: 500,
+  bold: 700,
+ },
+ lineHeights: {
+  body: 1.5,
+  heading: 1.125,
+ },
+ borders: {
+  none: "none",
+  box: "2px solid #F59256",
+ },
+ radii: {
+  none: "0",
+  normal: "40px",
+  round: "50%",
+ },
+};
+
+export default theme;
