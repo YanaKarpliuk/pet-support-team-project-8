96c04d460c209008df8e070e45b0c7d9c06496b6
Alona-Liubchenko <aylin888@rambler.ru>
2023-02-08 15:44:37 +0200

    authNav open

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,10 @@
 import { useState } from "react";
 import Logo from "../Logo/Logo";
 import Nav from "../Nav/Nav";
-// import AuthNav from "../AuthNav/AuthNav";
+import AuthNav from "../AuthNav/AuthNav";
 import { Box, MobileBtn, Menu, MenuNav } from "./Header.styled";
 import { IoMenu, IoClose } from "react-icons/io5";
-import UserNav from "../UserNav/UserNav";
+// import UserNav from "../UserNav/UserNav";
 const Header = () => {
     const [open, setOpen] = useState(false);
     const notOpen = () => setOpen(!open);
@@ -16,9 +16,9 @@ const Header = () => {
    </MenuNav>
 
    <Menu open={open}>
-    <UserNav noOpen={notOpen} />
+    {/* <UserNav noOpen={notOpen} /> */}
     {/* {isLoggedIn ? <UserNav /> : <AuthNav />} */}
-    {/* <AuthNav noOpen={notOpen} /> */}
+    <AuthNav noOpen={notOpen} />
    </Menu>
    <MobileBtn onClick={() => setOpen(!open)}>
     {open ? <IoClose size={40} /> : <IoMenu size={40} />}
