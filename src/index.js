eb1b2079acd88dc6d28f151e57cefb2e7e7ae3f5
Yana Karpliuk <yanakarpliuk99@gmail.com>
2023-02-08 20:32:13 +0200

    changed basename

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
  <React.StrictMode>
-  <BrowserRouter basename="/">
+  <BrowserRouter basename="/pet-support-team-project-8">
    <ThemeProvider theme={theme}>
     <App />
    </ThemeProvider>
