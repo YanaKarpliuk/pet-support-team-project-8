bc0dd634651875884f298eb0ca4c159f006b9fbf
Yana Karpliuk <yanakarpliuk99@gmail.com>
2023-02-08 18:29:36 +0200

    changed to hashrouter

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
-import './index.css';
+import "./index.css";
 
-import theme  from "./theme";
-import App from './App';
+import theme from "./theme";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
- <React.StrictMode>
-  <BrowserRouter basename="/">
-   <ThemeProvider theme={theme}>
-    <App />
-   </ThemeProvider>
-  </BrowserRouter>
- </React.StrictMode>,
+  <React.StrictMode>
+    <HashRouter>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </HashRouter>
+  </React.StrictMode>
 );
