53baa634fbb9abcc5cf0e42fd43bbd8e071ac2c2
Yana Karpliuk <yanakarpliuk99@gmail.com>
2023-02-14 13:35:07 +0200

    added content to friends

--- build/static/js/75.66dbb1ea.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkpet_support_team_project_8=self.webpackChunkpet_support_team_project_8||[]).push([[75],{3075:function(e,t,n){n.r(t);var r=n(3329);t.default=function(){return(0,r.jsx)("div",{style:{display:"flex",alignItems:"center",justifyContent:"center",height:"85vh"},children:(0,r.jsx)("h1",{style:{textAlign:"center"},children:"UserPage"})})}}}]);
-//# sourceMappingURL=75.66dbb1ea.chunk.js.map
