0b8422b62709a9124c22b287464733e6fb80cfdb
Gnatannn <96788997+Gnatannn@users.noreply.github.com>
2023-02-16 04:48:01 +0200

    7

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,12 +1,11 @@
 import { useSelector } from 'react-redux';
 import authSelectors from '../redux/auth/authSelectors';
 
-const { selectUser, selectIsRefreshing, selectIsLoggedIn } = authSelectors;
+const { selectUser, selectIsLoading, selectIsLoggedIn } = authSelectors;
 const useAuth = () => {
   const isLoggedIn = useSelector(selectIsLoggedIn);
   const user = useSelector(selectUser);
-  const isRefreshing = useSelector(selectIsRefreshing);
-  console.log(isLoggedIn);
+  const isRefreshing = useSelector(selectIsLoading);
   return { isLoggedIn, isRefreshing, user };
 };
 
