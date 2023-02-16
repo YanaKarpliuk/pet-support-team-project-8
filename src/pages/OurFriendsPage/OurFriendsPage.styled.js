cb0308f7aa65e651030339b9b807d64c4ea5ce41
Yana Karpliuk <yanakarpliuk99@gmail.com>
2023-02-16 18:22:12 +0200

    new attempt to fix styles in safari

--- src/pages/OurFriendsPage/OurFriendsPage.styled.js
+++ src/pages/OurFriendsPage/OurFriendsPage.styled.js
@@ -2,6 +2,30 @@ import styled from 'styled-components';
 import theme from '../../theme';
 import { Link } from 'react-router-dom';
 
+// const Title = styled.h2`
+//   margin-top: 42px;
+//   margin-bottom: 28px;
+//   font-family: ${theme.fonts.manrope};
+//   font-weight: 700;
+//   font-size: 24px;
+//   line-height: 33px;
+//   text-align: center;
+
+//   @media screen and ((max-width: 1279px) and (min-width: 768px)) {
+//     font-size: 48px;
+//     line-height: 66px;
+//     margin-top: 88px;
+//     margin-bottom: 40px;
+//   }
+
+//   @media screen and ((min-width: 1280px)) {
+//     font-size: 48px;
+//     line-height: 66px;
+//     margin-top: 72px;
+//     margin-bottom: 60px;
+//   }
+// `;
+
 const Title = styled.h2`
   margin-top: 42px;
   margin-bottom: 28px;
@@ -11,14 +35,14 @@ const Title = styled.h2`
   line-height: 33px;
   text-align: center;
 
-  @media screen and ((max-width: 1279px) and (min-width: 768px)) {
+  @media (768px <= width <= 1279px) {
     font-size: 48px;
     line-height: 66px;
     margin-top: 88px;
     margin-bottom: 40px;
   }
 
-  @media screen and ((min-width: 1280px)) {
+  @media (width >= 1280px) {
     font-size: 48px;
     line-height: 66px;
     margin-top: 72px;
@@ -26,37 +50,90 @@ const Title = styled.h2`
   }
 `;
 
+// const Container = styled.div`
+//   background-color: ${theme.colors.bg};
+//   padding: 20px 20px 100px;
+//   margin-top: 67px;
+
+//   @media screen and ((max-width: 1279px) and (min-width: 768px)) {
+//     margin-top: 82px;
+//   }
+
+//   @media screen and ((min-width: 1280px)) {
+//     margin-top: 82px;
+//   }
+// `;
+
+
 const Container = styled.div`
   background-color: ${theme.colors.bg};
   padding: 20px 20px 100px;
   margin-top: 67px;
 
-  @media screen and ((max-width: 1279px) and (min-width: 768px)) {
+  @media (768px <= width <= 1279px) {
     margin-top: 82px;
   }
 
-  @media screen and ((min-width: 1280px)) {
+  @media (width >= 1280px) {
     margin-top: 82px;
   }
 `;
+
+// const List = styled.ul`
+//   list-style: none;
+//   max-width: 320px;
+//   margin: 0 auto;
+//   @media screen and ((max-width: 1279px) and (min-width: 768px)) {
+//     display: grid;
+//     grid-template-columns: 1fr 1fr;
+//     gap: 32px;
+//     max-width: 768px;
+//   }
+//   @media screen and ((min-width: 1280px)) {
+//     display: grid;
+//     grid-template-columns: 1fr 1fr 1fr;
+//     gap: 32px;
+//     max-width: 1280px;
+//   }
+// `;
 
 const List = styled.ul`
   list-style: none;
   max-width: 320px;
   margin: 0 auto;
-  @media screen and ((max-width: 1279px) and (min-width: 768px)) {
+  @media (768px <= width <= 1279px) {
     display: grid;
     grid-template-columns: 1fr 1fr;
     gap: 32px;
     max-width: 768px;
   }
-  @media screen and ((min-width: 1280px)) {
+  @media (width >= 1280px) {
     display: grid;
     grid-template-columns: 1fr 1fr 1fr;
     gap: 32px;
     max-width: 1280px;
   }
 `;
+
+// const Card = styled.li`
+//   padding: 14px 4px 12px;
+//   background-color: ${theme.colors.white};
+//   border-radius: 20px;
+//   box-shadow: 7px 4px 14px rgba(49, 21, 4, 0.07);
+//   &:not(:last-child) {
+//     margin-bottom: 12px;
+//   }
+//   @media screen and ((max-width: 1279px) and (min-width: 768px)) {
+//     &:not(:last-child) {
+//       margin-bottom: 0;
+//     }
+//   }
+//   @media screen and ((min-width: 1280px)) {
+//     &:not(:last-child) {
+//       margin-bottom: 0;
+//     }
+//   }
+// `;
 
 const Card = styled.li`
   padding: 14px 4px 12px;
@@ -66,12 +143,12 @@ const Card = styled.li`
   &:not(:last-child) {
     margin-bottom: 12px;
   }
-  @media screen and ((max-width: 1279px) and (min-width: 768px)) {
+  @media (768px <= width <= 1279px) {
     &:not(:last-child) {
       margin-bottom: 0;
     }
   }
-  @media screen and ((min-width: 1280px)) {
+  @media (width >= 1280px) {
     &:not(:last-child) {
       margin-bottom: 0;
     }
@@ -82,6 +159,23 @@ const Box = styled.div`
   display: flex;
 `;
 
+// const ContentBox = styled.div`
+//   font-family: ${theme.fonts.manrope};
+//   font-size: 12px;
+//   color: ${theme.colors.black};
+//   font-weight: ${theme.fontWeights.normal};
+//   line-height: 16px;
+
+//   @media screen and ((max-width: 1279px) and (min-width: 768px)) {
+//     line-height: 19px;
+//     font-size: 14px;
+//   }
+//   @media screen and ((min-width: 1280px)) {
+//     line-height: 22px;
+//     font-size: 16px;
+//   }
+// `;
+
 const ContentBox = styled.div`
   font-family: ${theme.fonts.manrope};
   font-size: 12px;
@@ -89,15 +183,41 @@ const ContentBox = styled.div`
   font-weight: ${theme.fontWeights.normal};
   line-height: 16px;
 
-  @media screen and ((max-width: 1279px) and (min-width: 768px)) {
+  @media (768px <= width <= 1279px) {
     line-height: 19px;
     font-size: 14px;
   }
-  @media screen and ((min-width: 1280px)) {
+  @media (width >= 1280px) {
     line-height: 22px;
     font-size: 16px;
   }
 `;
+
+// const ImageBox = styled.div`
+//   margin-right: 12px;
+//   width: 110px;
+//   height: 80px;
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+
+//   & > img {
+//     height: 100%;
+//     width: 100%;
+//   }
+
+//   @media screen and ((max-width: 1279px) and (min-width: 768px)) {
+//     width: 120px;
+//     height: 85px;
+//     margin-right: 14px;
+//   }
+
+//   @media screen and ((min-width: 1280px)) {
+//     width: 158px;
+//     height: 112px;
+//     margin-right: 16px;
+//   }
+// `;
 
 const ImageBox = styled.div`
   margin-right: 12px;
@@ -112,18 +232,41 @@ const ImageBox = styled.div`
     width: 100%;
   }
 
-  @media screen and ((max-width: 1279px) and (min-width: 768px)) {
+  @media (768px <= width <= 1279px) {
     width: 120px;
     height: 85px;
     margin-right: 14px;
   }
 
-  @media screen and ((min-width: 1280px)) {
+  @media (width >= 1280px) {
     width: 158px;
     height: 112px;
     margin-right: 16px;
   }
 `;
+
+// const TitleLink = styled(Link)`
+//   font-family: ${theme.fonts.manrope};
+//   font-size: 12px;
+//   display: block;
+//   color: ${theme.colors.accent};
+//   font-weight: ${theme.fontWeights.bold};
+//   line-height: 16px;
+//   text-decoration-line: underline;
+//   margin-bottom: 10px;
+//   text-align: center;
+
+//   @media screen and ((max-width: 1279px) and (min-width: 768px)) {
+//     margin-bottom: 16px;
+//     line-height: 19px;
+//     font-size: 14px;
+//   }
+//   @media screen and ((min-width: 1280px)) {
+//     margin-bottom: 16px;
+//     line-height: 22px;
+//     font-size: 16px;
+//   }
+// `;
 
 const TitleLink = styled(Link)`
   font-family: ${theme.fonts.manrope};
@@ -136,12 +279,12 @@ const TitleLink = styled(Link)`
   margin-bottom: 10px;
   text-align: center;
 
-  @media screen and ((max-width: 1279px) and (min-width: 768px)) {
+  @media (768px <= width <= 1279px) {
     margin-bottom: 16px;
     line-height: 19px;
     font-size: 14px;
   }
-  @media screen and ((min-width: 1280px)) {
+  @media (width >= 1280px) {
     margin-bottom: 16px;
     line-height: 22px;
     font-size: 16px;
@@ -162,17 +305,34 @@ const ContentA = styled.a`
   }
 `;
 
+// const ContentItem = styled.div`
+//   &:not(:last-child) {
+//     margin-bottom: 4px;
+//   }
+
+//   @media screen and ((max-width: 1279px) and (min-width: 768px)) {
+//     &:not(:last-child) {
+//       margin-bottom: 8px;
+//     }
+//   }
+//   @media screen and ((min-width: 1280px)) {
+//     &:not(:last-child) {
+//       margin-bottom: 12px;
+//     }
+//   }
+// `;
+
 const ContentItem = styled.div`
   &:not(:last-child) {
     margin-bottom: 4px;
   }
 
-  @media screen and ((max-width: 1279px) and (min-width: 768px)) {
+  @media (768px <= width <= 1279px) {
     &:not(:last-child) {
       margin-bottom: 8px;
     }
   }
-  @media screen and ((min-width: 1280px)) {
+  @media (width >= 1280px) {
     &:not(:last-child) {
       margin-bottom: 12px;
     }
@@ -192,6 +352,28 @@ const ContentItemModal = styled.ul`
   width: 120px;
 `;
 
+// const ContentItemTime = styled.div`
+//   margin-bottom: 4px;
+//   cursor: pointer;
+//   position: relative;
+
+//   &:hover {
+//     color: ${theme.colors.accent};
+//   }
+
+//   &:hover > ${ContentItemModal} {
+//     display: flex;
+//   }
+
+//   @media screen and ((max-width: 1279px) and (min-width: 768px)) {
+//     margin-bottom: 8px;
+//   }
+
+//   @media screen and ((min-width: 1280px)) {
+//     margin-bottom: 12px;
+//   }
+// `;
+
 const ContentItemTime = styled.div`
   margin-bottom: 4px;
   cursor: pointer;
@@ -205,11 +387,11 @@ const ContentItemTime = styled.div`
     display: flex;
   }
 
-  @media screen and ((max-width: 1279px) and (min-width: 768px)) {
+  @media (768px <= width <= 1279px) {
     margin-bottom: 8px;
   }
 
-  @media screen and ((min-width: 1280px)) {
+  @media (width >= 1280px) {
     margin-bottom: 12px;
   }
 `;
