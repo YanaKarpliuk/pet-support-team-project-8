7b8e1560ebd2a6482f7a675a94f0594e7483a0bf
Yana Karpliuk <yanakarpliuk99@gmail.com>
2023-02-21 17:17:28 +0200

    added hashrouter

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {
-  BrowserRouter,
-} from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import './index.css';
 import { Provider } from 'react-redux';
@@ -17,11 +15,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter>
+      <HashRouter>
         <ThemeProvider theme={theme}>
           <App />
         </ThemeProvider>
-      </BrowserRouter>
+      </HashRouter>
     </PersistGate>
   </Provider>
 );
